7a02615d01bef20855d962a0ccc2d124d23b6c08
rjmacarthy <richardmacarthy@hotmail.com>
2025-03-07 23:19:32 +0000

    add deepseek

--- src/handlers/utils.ts
+++ src/handlers/utils.ts
@@ -120,6 +120,16 @@ export const Handlers: Record<string, (opts: ConfigOptions) => any> = {
       models.openrouter.supportsN,
       models.openrouter.supportsStreaming
     ),
+  ['deepseek']: (opts: ConfigOptions) =>
+    new OpenAICompatibleHandler(
+      opts,
+      models.deepseek.models,
+      models.deepseek.supportsJSON,
+      models.deepseek.supportsImages,
+      models.deepseek.supportsToolCalls,
+      models.deepseek.supportsN,
+      models.deepseek.supportsStreaming
+    ),
   ['openai-compatible']: (opts: ConfigOptions) =>
     new OpenAICompatibleHandler(
       opts,
